a889df47f10e432c2b8a9d62cb6d3f225d1af6a5
vivektemptest <vivektemptest@gmail.com>
2024-04-12 00:43:48 +0530

    new react larvel begib

---
+++ frontend/src/router.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import { createBrowserRouter,Navigate } from "react-router-dom";
+import App from "./App";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Surveys from "./pages/Surveys";
+import GuestLayout from "./layouts/GuestLayout";
+import DefualtLayout from "./layouts/DefaultLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefualtLayout />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Navigate to="/" />,
+      },
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/surveys",
+        element: <Surveys />,
+      },
+    ],
+  },
+
+  {
+    path: "/",
+    element: <GuestLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
+
+export default router;
